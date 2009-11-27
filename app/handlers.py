b3b49c6be48f0dd7e4edbf44a45596e70b4c06ed
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2009-11-27 10:23:09 +0530

    Add a handler for the privacy page

--- app/handlers.py
+++ app/handlers.py
@@ -44,9 +44,17 @@ class IndexHandler(webapp.RequestHandler):
         response = render_template('index.html')
         self.response.out.write(response)
 
+class PrivacyHandler(webapp.RequestHandler):
+    """Handler for the privacy page."""
+    def get(self):
+        response = render_template('privacy.html')
+        self.response.out.write(response)
+
+
 # URL-to-request-handler mappings.
 urls = (
     ('/', IndexHandler),
+    ('/privacy/?', PrivacyHandler),
 )
 
 # Web application entry-point.
