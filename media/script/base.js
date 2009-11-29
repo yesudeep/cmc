9580980b1f31158d217a303b6436054b38d618d5
Yesudeep Mangalapilly <yesudeep@mia.local>
2009-11-30 03:06:09 +0530

    get the initial look of the landing page up

--- media/script/base.js
+++ media/script/base.js
@@ -1,10 +1,10 @@
 jQuery(function(){
-    var elements = {
-    external_links: jQuery('a[href^="http://"]'),
-    email_links: jQuery('a[href^="mailto:"]')
-    };
+           var elements = {
+               external_links: jQuery('a[href^="http://"]'),
+               email_links: jQuery('a[href^="mailto:"]')
+           };
+           
+           elements.external_links.attr('target', '_blank').attr('title', 'Opens the link a new tab/window.');
+           elements.email_links.attr('title', 'Opens your email client to send an email.');
+       });
 
-    elements.external_links.attr('target', '_blank').attr('title', 'Opens the link a new tab/window.');
-    elements.email_links.attr('title', 'Opens your email client to send an email.');
-
-  });
---
+++ media/script/index.js
@@ -0,0 +1,13 @@
+jQuery(function(){
+           jQuery("#tweets").tweet({
+                                 username: "yesudeep",
+                                 join_text: "auto",
+                                 count: 3,
+                                 auto_join_text_default: "",
+                                 auto_join_text_ed: "I",
+                                 auto_join_text_ing: "was",
+                                 auto_join_text_reply: "replied to",
+                                 auto_join_text_url: "was checking out",
+                                 loading_text: "twittering..."
+                             });
+       });
