1fcbba290666a25685e61be73efd9e41ccafe7d1
Yesudeep Mangalapilly <yesudeep@mia.local>
2009-12-05 12:34:05 +0530

    get facebook connect working

--- media/script/start.js
+++ media/script/start.js
@@ -1,3 +1,23 @@
+/*
+ * Facebook connect integration
+ */
+function fbUpdateUserBox(){
+    var userBox = document.getElementById("user-box");
+    userBox.innerHTML = "<span>"
+        + "<fb:profile-pic uid='loggedinuser' facebook-logo='true'></fb:profile-pic>"
+        + "Welcome, <fb:name uid='loggedinuser' useyou='false'></fb:name>. "
+        + "You are signed in with your Facebook account."
+        + "</span>";
+    FB.XFBML.Host.parseDomTree();
+}
+FB.init(window.fb.api_key, window.fb.cross_domain_receiver_url, {
+        "ifUserConnected": fbUpdateUserBox,
+            "reloadIfSessionStateChanged": true
+            });
+
+/*
+ * Other stuff.
+ */
 jQuery(function(){
 
     });
