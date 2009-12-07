234f1fde455cb4af8571a7854200660f53735633
Yesudeep Mangalapilly <yesudeep@mia.local>
2009-12-07 16:57:20 +0530

    remove unnecessary code from base.js

--- media/script/base.js
+++ media/script/base.js
@@ -4,16 +4,25 @@
 jQuery(function(){
     var elements = {
         external_links: jQuery('a[href^="http://"]'),
-        email_links: jQuery('a[href^="mailto:"]'),
         exposables: jQuery('.portrait, .polaroids, .a2a_menu')
     };
 
     elements.exposables.hover(function(){
-        jQuery(this).expose({api:true, opacity: 0.6, loadSpeed: 100, color: '#000'}).load();
+        jQuery(this)
+            .expose({
+                api:true,
+                opacity: 0.6,
+                loadSpeed: 100,
+                color: '#000'})
+            .load();
     }, function(){
-        jQuery(this).expose({api:true, closeSpeed: 100}).close();
+        jQuery(this)
+            .expose({
+                api:true,
+                closeSpeed: 100
+            })
+            .close();
     });
-    elements.external_links.attr('target', '_blank').attr('title', 'Opens the link a new tab/window.');
-    elements.email_links.attr('title', 'Opens your email client to send an email.');
+    elements.external_links.attr('target', '_blank');
 });
 
