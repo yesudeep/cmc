e8185d9ee348e9f6932f5fab8d00a8c6011f8ffd
Yesudeep Mangalapilly <yesudeep@mia.local>
2010-01-16 17:32:42 +0530

    update doc in configuration.py and use assets.cuttingmasalachai.com

--- app/configuration.py
+++ app/configuration.py
@@ -42,6 +42,19 @@
 # App Engine does not send a "304 not modified" HTTP status for
 # static files and hence these files do not get cached by the browser.
 # And serving static files without caching results in a slow Website.
+#
+# Why use App Engine to serve static files?
+# -----------------------------------------
+#
+#    1. Google CDN.
+#    2. Browser based cache works (partially)
+#
+#  Disadvantages:
+#
+#    1. Does not send a 304 Not modified header.
+#    2. Every domain has a limit on the number of connections.
+#       Separating media into a subdomain lets the browser
+#       open more connections and download more media simultaneously.
 
 import sys
 import os
@@ -105,8 +118,11 @@ else:
     HOST_NAME = SERVER_NAME
     LOCAL = False
     DEBUG = False
-    MEDIA_URL = "http://static.%s/u/3274846/public/" % (NAKED_DOMAIN, )
+    #MEDIA_URL = "http://static.%s/u/3274846/public/" % (NAKED_DOMAIN, )
     TEXT_MEDIA_URL = "http://assets.%s/" % (NAKED_DOMAIN, )
+    MEDIA_URL = TEXT_MEDIA_URL
+    #MEDIA_URL = 'http://%s/s/' % (HOST_NAME, )
+    #TEXT_MEDIA_URL = MEDIA_URL
 
 if DEBUG:
     # Minification suffixes to use for CSS and JS files.
